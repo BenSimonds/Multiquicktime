27ffb15aba67b19b51c5d26e55b6492659468834
Ben Simonds <BSSimonds@gmail.com>
2014-04-08 15:03:05 +0100

    First attempt at splitting into __init__ and the rest.

--- Multiquicktime.py
+++ Multiquicktime.py
@@ -1,15 +1,22 @@
-bl_info = {
-    "name": "Multi Quicktime",
-    "author": "Ben Simonds",
-    "version": (1, 12),
-    "blender": (2, 69, 0),
-    "location": "Properties > Render > MultiQuicktime",
-    "description": "Automatically Converts an Image Sequence to multiple quicktime movies once a render finishes.",
-    "warning": "Requires qt_tools and ffmpeg",
-    "wiki_url": "",
-    "tracker_url": "",
-    "category": "Render",
-    }
+# ***** BEGIN GPL LICENSE BLOCK *****
+#
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software Foundation,
+# Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+#
+# ***** END GPL LICENCE BLOCK *****
+
 
 
 import bpy
@@ -17,9 +24,40 @@ import os, json
 from bpy.app.handlers import persistent
 from bpy.types import Menu, Panel, UIList
 
+# Properties Classes
 
-#First set up the most important functions and classes. Namely Do_Dialog (with user defined export location), and Autoquicktime (again with selectable export settings.
+class MultiQuicktimes(bpy.types.PropertyGroup): #This holds the list of quicktimes to make.
+    name = bpy.props.StringProperty(name="Filename", default = "Quicktime")
+    settings = bpy.props.StringProperty(name="Settings",subtype='FILE_PATH', default = ((os.path.realpath(__file__).rsplit("/",1)[0]) + "/multiqtsettings/default.st"))
+    mute = bpy.props.BoolProperty(name="Mute", default=True)
+    overridefps = bpy.props.BoolProperty(name = "Override FPS", default = False)
+    sequencerate = bpy.props.FloatProperty(name = "Sequence Rate", default = 25) #bpy.context.scene.render.fps) #Why wont this work?
+    auto_open = bpy.props.BoolProperty(name="Open When Done", default = False)
+    #Watermark Stuff
+    #Video
+    watermark = bpy.props.BoolProperty(name = "Watermark Video", default=False)
+    watermark_file = bpy.props.StringProperty(name="Watermark Image",subtype = 'FILE_PATH',default = ((os.path.realpath(__file__).rsplit("/",1)[0]) + "/multiqtsettings/WatermarkDefault.png"))
+    #Frame
+    watermark_preview = bpy.props.BoolProperty(name = "Watermark Frame", default = False)
+    watermark_frame = bpy.props.IntProperty(name="Preview Frame", default = 1)
+    watermark_frame_file = bpy.props.StringProperty(name="Watermark Frame Image",subtype = 'FILE_PATH',default = ((os.path.realpath(__file__).rsplit("/",1)[0]) + "/multiqtsettings/WatermarkFrameDefault.png")) 
+    
 
+class MultiQuicktimeProps(bpy.types.PropertyGroup): #Class to hold basic props, plus functions to create new ones?
+    multiquicktime_on = bpy.props.BoolProperty(
+        name = "Multi-Quicktime",
+        description = "Automatically generate multiple quicktime files once render finishes.",
+        default = False)
+    multiquicktime_overwrite = bpy.props.BoolProperty(
+        name = "Overwrite",
+        description = "Overwrite Existing Files. WARNING: Will not overwrite if a file is open in quicktime or elsewhere.",
+        default = True)
+    quicktimes = bpy.props.CollectionProperty(type=MultiQuicktimes)
+    output_dir = bpy.props.StringProperty(name="Output Directory", subtype='DIR_PATH',description="Directory to send quicktime files to.", default = bpy.context.user_preferences.filepaths.temporary_directory)
+    active_quicktime = bpy.props.IntProperty(name="Active Quicktime", default = 0)
+    
+
+# Functions and Classes:
 
 def dodialog_save(filename):
         #Settings Path
@@ -146,6 +184,8 @@ class RenderedFrames:
         self.firstframe = frames[0]
         self.lastframe = frames[-1]
 
+#Operators:
+
 class DoDialog(bpy.types.Operator):
     """Set up export settings for quicktime auto export."""
     bl_idname = "render.do_dialog"
@@ -202,9 +242,8 @@ class DelQT(bpy.types.Operator):
             bpy.context.scene.multi_quicktime.active_quicktime = 0
         return{"FINISHED"}
 
-# Version 1.11: Ability to import/export a text file with a list of quicktimes to create.
 
-# Export:
+# Import/Export:
 
 class ExportQTList(bpy.types.Operator):
     bl_idname = "render.export_qtlist"
@@ -297,148 +336,7 @@ class ImportQTList(bpy.types.Operator):
         context.window_manager.fileselect_add(self)
         return {'RUNNING_MODAL'}
     
-
-#We need a user creatable list of quicktimes to create, each with a user defined settings file.
-
-
-class MultiQuicktimes(bpy.types.PropertyGroup): #This holds the list of quicktimes to make.
-    name = bpy.props.StringProperty(name="Filename", default = "Quicktime")
-    settings = bpy.props.StringProperty(name="Settings",subtype='FILE_PATH', default = ((os.path.realpath(__file__).rsplit("/",1)[0]) + "/multiqtsettings/default.st"))
-    mute = bpy.props.BoolProperty(name="Mute", default=True)
-    overridefps = bpy.props.BoolProperty(name = "Override FPS", default = False)
-    sequencerate = bpy.props.FloatProperty(name = "Sequence Rate", default = 25) #bpy.context.scene.render.fps) #Why wont this work?
-    auto_open = bpy.props.BoolProperty(name="Open When Done", default = False)
-    #Watermark Stuff
-    #Video
-    watermark = bpy.props.BoolProperty(name = "Watermark Video", default=False)
-    watermark_file = bpy.props.StringProperty(name="Watermark Image",subtype = 'FILE_PATH',default = ((os.path.realpath(__file__).rsplit("/",1)[0]) + "/multiqtsettings/WatermarkDefault.png"))
-    #Frame
-    watermark_preview = bpy.props.BoolProperty(name = "Watermark Frame", default = False)
-    watermark_frame = bpy.props.IntProperty(name="Preview Frame", default = 1)
-    watermark_frame_file = bpy.props.StringProperty(name="Watermark Frame Image",subtype = 'FILE_PATH',default = ((os.path.realpath(__file__).rsplit("/",1)[0]) + "/multiqtsettings/WatermarkFrameDefault.png")) 
-    
-
-class MultiQuicktimeProps(bpy.types.PropertyGroup): #Class to hold basic props, plus functions to create new ones?
-    multiquicktime_on = bpy.props.BoolProperty(
-        name = "Multi-Quicktime",
-        description = "Automatically generate multiple quicktime files once render finishes.",
-        default = False)
-    multiquicktime_overwrite = bpy.props.BoolProperty(
-        name = "Overwrite",
-        description = "Overwrite Existing Files. WARNING: Will not overwrite if a file is open in quicktime or elsewhere.",
-        default = True)
-    quicktimes = bpy.props.CollectionProperty(type=MultiQuicktimes)
-    output_dir = bpy.props.StringProperty(name="Output Directory", subtype='DIR_PATH',description="Directory to send quicktime files to.", default = bpy.context.user_preferences.filepaths.temporary_directory)
-    active_quicktime = bpy.props.IntProperty(name="Active Quicktime", default = 0)
-    
-    
-# Now create the UI Stuff
-
-class MULTIQUICKTIME_UL_quicktimes(UIList):
-    def draw_item(self, context, layout, data, item, icon, active_data, active_propname, index):
-        # assert(isinstance(item, bpy.types.SceneRenderLayer)
-        quicktime = item
-        layout.label(quicktime.name, icon='RENDER_ANIMATION', translate=False)
-        layout.prop(quicktime, "mute", text="", index=index)
-        #layout.prop(quicktime, "sequencerate", text="", index=index)
-
-
-
-class MultiQuicktimePanel(Panel):
-    """Creates the UI Panel for the MultiQuicktime settings."""
-    bl_label = "Multi-Quicktime"
-    bl_idname = "OBJECT_PT_multiquicktime"
-    bl_space_type = 'PROPERTIES'
-    bl_region_type = 'WINDOW'
-    bl_context = "render"
-    
-    def draw(self,context):
-        scene = bpy.context.scene
-        mqt = scene.multi_quicktime
-        layout = self.layout
-        
-        
-        #Normal settings (global to all exports).
-        row = layout.row()
-        row.prop(mqt, 'multiquicktime_on', text = "Auto Generate After Render")
-        row.prop(mqt, 'multiquicktime_overwrite')
-        
-        row = layout.row()
-        row.prop(mqt, 'output_dir')
-        
-        row = layout.row()
-        row.label(text = "Outputs:")
-        
-        col = row.column()
-        row = col.row(align = True)
-        row.operator('render.export_qtlist', text = "Export List", icon = "EXPORT")
-        row.operator('render.import_qtlist', text = "Import List", icon = "IMPORT")
-        
-        if len(mqt.quicktimes.items()) == 0:
-            row = layout.row()
-            row.operator('render.add_multi_quicktime', text = "Add Multi Quicktime Output", icon ='ZOOMIN')
-        else:
-            #Make Sure active quicktime is a valid number.
-            if mqt.active_quicktime < len(mqt.quicktimes):
-                quicktime = mqt.quicktimes[mqt.active_quicktime]
-            else:
-                quicktime = mqt.quicktimes[0]
-
-        
-            row = layout.row()
-            row.template_list('MULTIQUICKTIME_UL_quicktimes', 'MultiQuicktimes', bpy.context.scene.multi_quicktime, "quicktimes", bpy.context.scene.multi_quicktime, "active_quicktime")
-            
-            
-            col = row.column(align=True)
-            col.operator('render.add_multi_quicktime', text = "", icon ='ZOOMIN')
-            col.operator('render.delete_multi_quicktime', text = "", icon = 'ZOOMOUT').to_remove = quicktime.name
-            
-            
-            row = layout.row()
-            row.separator()
            
-            row = layout.row()
-            row.prop(quicktime,'name',text = "Name")
-            
-            row = layout.row()
-            row.prop(quicktime,'auto_open')
-            #row.prop(quicktime,'mute',text = "Mute")
-                
-            row = layout.row(align = True)    
-            row.prop(quicktime, 'overridefps', text = "Override Sequence Rate")
-            col = row.column()
-            col.prop(quicktime,'sequencerate',text = "")
-            col.active = quicktime.overridefps
-                
-            row = layout.row(align = True)
-            row.prop(quicktime,'settings',text = "Settings File")
-            row.operator('render.do_dialog', text = "", icon = "SCRIPT").filename = quicktime.settings #Configure Button
-            
-            #Watermark Options
-            row = layout.row()
-            row.prop(quicktime, 'watermark')
-            if mqt.quicktimes[mqt.active_quicktime].watermark:
-                row.prop(quicktime, 'watermark_file')
-            
-            row = layout.row()
-            row.prop(quicktime, 'watermark_preview')
-            if mqt.quicktimes[mqt.active_quicktime].watermark_preview:    
-                row.prop(quicktime, 'watermark_frame')
-                row.prop(quicktime, 'watermark_frame_file')
-            
-            #Give user a flag if config file does not exist...
-            row = layout.row()
-            if os.path.isfile(bpy.path.abspath(quicktime.settings)) == False:
-                row.label(text = "WARNING: No config file found.", icon ='ERROR')
-        
-        
-        row = layout.row()
-        row.separator()
-    
-        row = layout.row()
-        row.operator('render.multi_quicktime', text = "Generate Outputs from Frames")
-        
-        
 #Handler code for auto sequencing after rendering.
 
 #handle = "" #XXX UGLY Dummy Variable.
@@ -476,53 +374,4 @@ def end_render(scene): #This gets assigned to render_complete
     animation_count = 0
     rendering_check = 0
     
-######
 
-def register():
-    #Properties:
-    bpy.utils.register_class(MultiQuicktimes)
-    bpy.utils.register_class(MultiQuicktimeProps)    
-    bpy.types.Scene.multi_quicktime = bpy.props.PointerProperty(type=MultiQuicktimeProps)
-    #print(bpy.context.scene.multi_quicktime.multiquicktime_on)
-    #Operatorstypes.
-    bpy.utils.register_class(DoDialog)    
-    bpy.utils.register_class(MultiQuicktimeFrames)
-    bpy.utils.register_class(AddQT)
-    bpy.utils.register_class(RemoveQT)
-    bpy.utils.register_class(DelQT)
-    bpy.utils.register_class(ExportQTList)
-    bpy.utils.register_class(ImportQTList)
-    #UI Stuff:
-    bpy.utils.register_class(MultiQuicktimePanel)
-    bpy.utils.register_class(MULTIQUICKTIME_UL_quicktimes)
-    #Handlers:
-    bpy.app.handlers.render_pre.append(render_started)
-    bpy.app.handlers.render_cancel.append(kill_render)
-    bpy.app.handlers.render_complete.append(end_render)
-    bpy.app.handlers.frame_change_post.append(check_animation)
-    
-
-def unregister():
-    #Properties:
-    bpy.utils.unregister_class(MultiQuicktimes)
-    bpy.utils.unregister_class(MultiQuicktimeProps)    
-    #Operatorstypes.
-    bpy.utils.unregister_class(DoDialog)    
-    bpy.utils.unregister_class(MultiQuicktimeFrames)
-    bpy.utils.unregister_class(AddQT)
-    bpy.utils.unregister_class(RemoveQT)
-    bpy.utils.unregister_class(DelQT)
-    bpy.utils.unregister_class(ExportQTList)
-    bpy.utils.unregister_class(ImportQTList)
-    #UI Stuff:
-    bpy.utils.unregister_class(MultiQuicktimePanel)
-    bpy.utils.unregister_class(MULTIQUICKTIME_UL_quicktimes)
-    #Handlers:
-    bpy.app.handlers.render_pre.remove(render_started)
-    bpy.app.handlers.render_cancel.remove(kill_render)
-    bpy.app.handlers.render_complete.remove(end_render)
-    bpy.app.handlers.frame_change_post.remove(check_animation)
-
-    
-if __name__ == '__main__':
-    register()
